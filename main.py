6790c5fa1874754e99ed6e8aababfe35aa144031
PedroSmaxY <pedrohnovais@outlook.com>
2023-12-05 22:47:48 -0300

    Refactor calculator function and function names

--- main.py
+++ main.py
@@ -1,76 +1,74 @@
-from src.func_expo import funcao_exponencial
-from src.func_trig import funcao_trigonometrica
-from src.limpar_console import limpar_console
+from src.func_exp import exponential_function
+from src.func_trig import trigonometric_function
+from src.clear_console import clear_console
 
-
-def calculadora():
+def calculator():
     while True:
-        print("Bem-vindo à Calculadora com Gráfico!")
-        print("\nEscolha uma operação:\n")
-        print("1 - Adição")
-        print("2 - Subtração")
-        print("3 - Multiplicação")
-        print("4 - Divisão")
-        print("5 - Gráfico da Função Exponencial e Derivada/Integral")
-        print("6 - Gráfico da Função Trigonométrica e Derivada/Integral")
-        print("0 - Sair\n")
+        print("Welcome to the Graphing Calculator!")
+        print("\nChoose an operation:\n")
+        print("1 - Addition")
+        print("2 - Subtraction")
+        print("3 - Multiplication")
+        print("4 - Division")
+        print("5 - Exponential Function and Derivative/Integral Graph")
+        print("6 - Trigonometric Function and Derivative/Integral Graph")
+        print("0 - Exit\n")
 
-        escolha = input("Digite o número da operação desejada: ")
+        choice = input("Enter the number of the desired operation: ")
 
-        if escolha == '0':
-            limpar_console()
-            print("Saindo da calculadora. Até logo!")
+        if choice == '0':
+            clear_console()
+            print("Exiting the calculator. Goodbye!")
             break
-        elif escolha in ['1', '2', '3', '4']:
-            limpar_console()
-            operacao = int(escolha)
+        elif choice in ['1', '2', '3', '4']:
+            clear_console()
+            operation = int(choice)
             while True:
                 try:
-                    num1 = float(input("Digite o primeiro número: "))
-                    num2 = float(input("Digite o segundo número: "))
+                    num1 = float(input("Enter the first number: "))
+                    num2 = float(input("Enter the second number: "))
                     break
                 except ValueError:
-                    print("Erro: Entrada inválida!")
-                    input("Pressione ENTER para continuar...")
-                    limpar_console()
+                    print("Error: Invalid input!")
+                    input("Press ENTER to continue...")
+                    clear_console()
                     continue
 
-            if operacao == 1:
-                resultado = num1 + num2
-                print(f"\nResultado: {num1:.0f} + {num2:.0f} = {resultado:.0f}")
-                input("\nPressione ENTER para continuar...")
+            if operation == 1:
+                result = num1 + num2
+                print(f"\nResult: {num1:.0f} + {num2:.0f} = {result:.0f}")
+                input("\nPress ENTER to continue...")
                 
-            elif operacao == 2:
-                resultado = num1 - num2
-                print(f"\nResultado: {num1:.0f} - {num2:.0f} = {resultado:.0f}")
-                input("\nPressione ENTER para continuar...")
+            elif operation == 2:
+                result = num1 - num2
+                print(f"\nResult: {num1:.0f} - {num2:.0f} = {result:.0f}")
+                input("\nPress ENTER to continue...")
 
-            elif operacao == 3:
-                resultado = num1 * num2
-                print(f"\nResultado: {num1:.0f} x {num2:.0f} = {resultado:.0f}")
-                input("\nPressione ENTER para continuar...")
+            elif operation == 3:
+                result = num1 * num2
+                print(f"\nResult: {num1:.0f} x {num2:.0f} = {result:.0f}")
+                input("\nPress ENTER to continue...")
 
-            elif operacao == 4:
+            elif operation == 4:
                 if num2 != 0:
-                    resultado = num1 / num2
-                    print(f"\nResultado: {num1} / {num2} = {resultado:.2f}")
-                    input("\nPressione ENTER para continuar...")
+                    result = num1 / num2
+                    print(f"\nResult: {num1} / {num2} = {result:.2f}")
+                    input("\nPress ENTER to continue...")
                 else:
-                    print("\nErro: Divisão por zero!")
-                    input("\nPressione ENTER para continuar...")
+                    print("\nError: Division by zero!")
+                    input("\nPress ENTER to continue...")
 
-        elif escolha == '5':
-            funcao_exponencial()
+        elif choice == '5':
+            exponential_function()
 
-        elif escolha == '6':
-            funcao_trigonometrica()
+        elif choice == '6':
+            trigonometric_function()
         else:
-            limpar_console()
-            print("Opção inválida. Tente novamente.")
+            clear_console()
+            print("Invalid option. Please try again.")
             continue
 
-        limpar_console()
-
+        clear_console()
 
 if __name__ == "__main__":
-    calculadora()
+    calculator()
