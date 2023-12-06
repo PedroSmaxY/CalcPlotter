314082fbef0abaf74608e77aab0ab9e0cdbd56e9
PedroSmaxY <pedrohnovais@outlook.com>
2023-12-05 22:48:07 -0300

    Refactor trigonometric function and remove exponential function

--- src/func_trig.py
+++ src/func_trig.py
@@ -1,85 +1,86 @@
-# Importa as bibliotecas necessárias
+# Import necessary libraries
 from sympy import symbols, diff, integrate, sympify, lambdify
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 
-# Utiliza a interface gráfica Tkinter para a criação da figura
-matplotlib.use('TkAgg') 
+# Use the Tkinter graphical interface to create the figure
+matplotlib.use('TkAgg')
 
-def funcao_trigonometrica():
-    limpar_console()
-    
-    # Define a variável simbólica x
+def trigonometric_function():
+    clear_console()
+
+    # Define the symbolic variable x
     x = symbols('x')
 
-    # Loop para garantir que o usuário insira uma função trigonométrica válida
+    # Loop to ensure the user inputs a valid trigonometric function
     while True:
-        print("Digite a função trigonométrica em termos de 'x' (por exemplo, sin(x))")
+        print("Enter the trigonometric function in terms of 'x' (e.g., sin(x))")
         try:
-            # Tenta converter a entrada do usuário em uma expressão simbólica
+            # Try to convert the user input into a symbolic expression
             expr = sympify(input("\ny="))
             break
         except:
-            # Se ocorrer um erro, exibe uma mensagem e continua o loop
-            limpar_console()
-            print("Erro: Função trigonométrica inválida. Certifique-se de usar a sintaxe correta.")
+            # If an error occurs, display a message and continue the loop
+            clear_console()
+            print("Error: Invalid trigonometric function. Make sure to use the correct syntax.")
             continue
 
-    # Calcula a derivada e a integral da função
-    derivada = diff(expr, x)
+    # Calculate the derivative and integral of the function
+    derivative = diff(expr, x)
     integral = integrate(expr, x)
 
-    # Exibe as informações da derivada e integral
-    print(f"\nDerivada da função: {derivada}\n")
-    print(f"Integral da função: {integral} + c\n")
+    # Display information about the derivative and integral
+    print(f"\nFunction's Derivative: {derivative}\n")
+    print(f"Function's Integral: {integral} + c\n")
 
-    # Converte a expressão da derivada em uma função numérica
-    derivada_func = lambdify(x, derivada, "numpy")
+    # Convert the derivative expression into a numeric function
+    derivative_func = lambdify(x, derivative, "numpy")
 
-    # Gera valores de x para o gráfico
+    # Generate x values for the plot
     x_vals = np.linspace(-100, 100, 10000)
 
-    # Avalia a expressão para obter os valores de y correspondentes
+    # Evaluate the expression to obtain corresponding y values
     y_vals_trig = [expr.subs(x, val) for val in x_vals]
 
-    # Avalia a função derivada nos valores de x para o gráfico
-    derivada_vals = derivada_func(x_vals)
+    # Evaluate the derivative function at x values for the plot
+    derivative_vals = derivative_func(x_vals)
 
-    # Define o tamanho da figura
+    # Set the figure size
     plt.figure(figsize=(10, 5))
 
-    # Define a funcão do gráfico
-    plt.plot(x_vals, y_vals_trig, label="Função Trigonométrica")
-    # Define a função da derivada d(x)
-    plt.plot(x_vals, derivada_vals, label="Derivada")
+    # Define the plot for the trigonometric function
+    plt.plot(x_vals, y_vals_trig, label="Trigonometric Function")
+    # Define the plot for the derivative d(x)
+    plt.plot(x_vals, derivative_vals, label="Derivative")
 
-    # Define a Largura e espessura dos eixos x e y
+    # Set the width and thickness of the x and y axes
     plt.axvline(0, color='black', linewidth=1.5)
     plt.axhline(0, color='black', linewidth=1.5)
 
-    # Define o nome da figura
-    plt.title("Gráfico da Função Trigonométrica")
-    # Define os nomes dos eixos x e y
+    # Set the title of the figure
+    plt.title("Trigonometric Function Graph")
+    # Set the names of the x and y axes
     plt.xlabel("x")
     plt.ylabel("y")
 
-    # Define a escala de visão do gráfico
+    # Set the view scale of the plot
     plt.xlim(-10, 10)
     plt.ylim(-5, 10)
 
-    # Adiciona os indices de cada função
+    # Add legends for each function
     plt.legend()
-    # Mostra a grade na figura
+    # Show the grid on the figure
     plt.grid(True)
-    
-    # Cria a figura
-    plt.show()
-    input("\nPressione ENTER para continuar...")
 
-# Executa a função principal se o script for executado como um programa independente
+    # Create the figure
+    plt.show()
+    input("\nPress ENTER to continue...")
+
+
+# Run the main function if the script is executed as an independent program
 if __name__ == "__main__":
-    from limpar_console import limpar_console
-    funcao_trigonometrica()
+    from clear_console import clear_console
+    trigonometric_function()
 else:
-    from .limpar_console import limpar_console
+    from .clear_console import clear_console
